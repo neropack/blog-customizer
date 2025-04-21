c18dd8db54f5d158284a9a1e2c1f73171c497990
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-04-21 13:36:15 +0300

    перенёс состояние формы выше по уровню

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -17,7 +17,7 @@ import { RadioGroup } from 'src/ui/radio-group';
 import { Separator } from 'src/ui/separator';
 import { Text } from 'src/ui/text';
 
-export const ArticleParamsForm = () => {
+export const ArticleParamsForm = (props: any) => {
 	const [isOpen, setIsOpen] = useState(false);
 	const rootRef = useRef<HTMLDivElement>(null);
 
@@ -39,34 +39,46 @@ export const ArticleParamsForm = () => {
 				className={clsx(styles.container, { [styles.container_open]: isOpen })}
 				ref={rootRef}>
 				<form className={styles.form}>
-					<Text as='h2' size={31} weight={800} uppercase={true}>
-						Задайте параметры
-					</Text>
-					{/* поменять selected */}
-					<Select selected={null} options={fontFamilyOptions} title='Шрифт' />
-					{/* поменять selected */}
-					<RadioGroup
-						name='font'
-						selected={fontSizeOptions[0]}
-						options={fontSizeOptions}
-						title='Размер шрифта'
-					/>
-					{/* поменять selected */}
-					<Select selected={null} options={fontColors} title='Цвет шрифта' />
+					<div className={styles.topContainer}>
+						<Text as='h2' size={31} weight={800} uppercase={true}>
+							Задайте параметры
+						</Text>
+						<Select
+							selected={props.state.fontFamilyOption}
+							options={fontFamilyOptions}
+							title='Шрифт'
+							onChange={props.handleChange('fontFamilyOption')}
+						/>
+						<RadioGroup
+							name='font'
+							selected={props.state.fontSizeOption}
+							options={fontSizeOptions}
+							title='Размер шрифта'
+							onChange={props.handleChange('fontSizeOption')}
+						/>
+						<Select
+							selected={props.state.fontColor}
+							options={fontColors}
+							title='Цвет шрифта'
+							onChange={props.handleChange('fontColor')}
+						/>
 
-					<Separator />
+						<Separator />
 
-					<Select
-						selected={null}
-						options={backgroundColors}
-						title='Цвет фона'
-					/>
+						<Select
+							selected={props.state.backgroundColor}
+							options={backgroundColors}
+							title='Цвет фона'
+							onChange={props.handleChange('backgroundColor')}
+						/>
 
-					<Select
-						selected={null}
-						options={contentWidthArr}
-						title='Ширина контента'
-					/>
+						<Select
+							selected={props.state.contentWidth}
+							options={contentWidthArr}
+							title='Ширина контента'
+							onChange={props.handleChange('contentWidth')}
+						/>
+					</div>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { defaultArticleState, OptionType } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,6 +13,20 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	// сделать состояние страницы
+	// перенести состояния сюда
+	const [formState, setFormState] = useState(defaultArticleState);
+
+	const handleChange = (name: string) => (selected: OptionType) => {
+		setFormState(
+			(prev) =>
+				(prev = {
+					...prev,
+					[name]: selected,
+				})
+		);
+	};
+	// надо как-то получить данные из формы
 	return (
 		<main
 			className={clsx(styles.main)}
@@ -25,7 +39,7 @@ const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm handleChange={handleChange} state={formState} />
 			<Article />
 		</main>
 	);
